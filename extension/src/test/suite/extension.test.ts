8dd8c7b8a122537aee73195fd11fdc8831dbd3e4
jinghongluo <jil040@ucsd.edu>
2020-05-11 16:29:38 -0700

    added github action for testing

--- extension/src/test/suite/extension.test.ts
+++ extension/src/test/suite/extension.test.ts
@@ -10,6 +10,13 @@ suite('Extension Test Suite', () => {
 
 	test('Sample test', () => {
 		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
+		assert.equal(-1, [1, 2, 3].indexOf(0))
+	});
+
+	test('Extension test', () => {
+		// TODO:
+		// do testing for extension such like given tester file, extenion output need to match the expected # of lines for file.
+
+		// do testing for extension such like given tester file, extension must outputs the correct file name
 	});
 });
