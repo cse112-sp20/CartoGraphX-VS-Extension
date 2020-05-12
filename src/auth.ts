fb1912d34de3010c0aa3abd427bdfff301f552b2
oscarvik <oscarcv@stud.ntnu.no>
2020-05-11 20:42:37 -0700

    refactor: moved firebase-config to seperate config file

--- src/auth.ts
+++ src/auth.ts
@@ -2,18 +2,6 @@ import * as firebase from 'firebase';
 import * as vscode from 'vscode';
 
 
-export const firebaseConfig = {
-    // place the config for your firebase application here
-    apiKey: "AIzaSyBoCc3PDlf5vQVgAS9iNlfoa8fMvbN-amY",
-    authDomain: "cgphx-56959.firebaseapp.com",
-    databaseURL: "https://cgphx-56959.firebaseio.com",
-    projectId: "cgphx-56959",
-    storageBucket: "cgphx-56959.appspot.com",
-    messagingSenderId: "962297814867",
-    appId: "1:962297814867:web:b4bd44e9ac216233f98ba6",
-    measurementId: "G-5VZCQLPBM4"
-};
-
 export const signIn = async (auth: firebase.auth.Auth) => {
     console.log('signing in...');
     const email = await vscode.window.showInputBox({placeHolder: 'email'});
