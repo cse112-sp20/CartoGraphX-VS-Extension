578f30ac3f92d1921234a0d0f0dd8d502fca667b
oscarvik <oscarcv@stud.ntnu.no>
2020-05-11 20:39:55 -0700

    moved content of auth-barnch from old repo

--- extension/src/main.ts
+++ extension/src/main.ts
@@ -1,19 +1,47 @@
 import * as vscode from 'vscode';
-import {displayCurrentWorkingFile} from './chartgraphx';
+import {displayCurrentWorkingFile, statusBarItem} from './chartgraphx';
+import * as firebase from 'firebase';
+import {firebaseConfig, signIn, signOut, userStatus} from './auth'; 
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
 
 export function activate(context: vscode.ExtensionContext) {
-	// This line of code will only be executed once when your extension is activated
 	console.log('ChartGraphX is now active!');
-
-	const statusBarItem = vscode.window.createStatusBarItem(undefined, 1000);
-	statusBarItem.command = 'chartGraphX.displayState';
-	statusBarItem.tooltip = "run CGphX command";
-	statusBarItem.text = "CGphX$(rocket)"; // to rotate icon ~spin
 	statusBarItem.show();
-	let disposable = vscode.commands.registerCommand('chartGraphX.displayState', () => {
-		displayCurrentWorkingFile();
-	}
-	);
+
+	let disposable = vscode.commands.registerCommand('chartGraphX.toolbarAction', async () => {
+		// shows quickpick with available commands depending on user is signed in or not
+		if (auth.currentUser !== null){
+			vscode.window.showQuickPick(
+				[
+					{ label: 'Sign out', description: 'Stop ChartGraphX tracking', target: signOut },
+					{ label: 'Get user info', undefined, target: userStatus },
+					{ label: 'Display Current Working File', undefined, target: displayCurrentWorkingFile }
+				],
+				{ placeHolder: 'ChartGraphX commands' }
+			).then( method =>{
+				method?.target(auth);
+			});
+		}
+		else{
+			vscode.window.showQuickPick(
+				[{ label: 'Email', description: 'Sign in using email and password', target: signIn }],
+				{ placeHolder: 'Select a signin method' }
+			).then( method =>{
+				method?.target(auth);
+			});
+		}	
+	});
+
+	// runs each time auth changes (login/logout)
+	auth.onAuthStateChanged(firebaseUser => {
+		if (firebaseUser === null) {statusBarItem.color = "darkgrey";}
+		else {statusBarItem.color = 'white';}
+		console.log(JSON.stringify(firebaseUser,null,2));
+	});
 	
 	context.subscriptions.push(disposable);
 }
@@ -21,3 +49,18 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+
+
+
+
+
+
+
+/*
+	// authentication commands
+	let signin = vscode.commands.registerCommand('chartGraphX.signIn', () => signIn(auth));
+
+	let signout = vscode.commands.registerCommand('chartGraphX.signOut', () => signOut(auth));
+	let checkAuthState = vscode.commands.registerCommand('chartGraphX.checkAuthState', () => console.log(auth.currentUser));
+*/ 
