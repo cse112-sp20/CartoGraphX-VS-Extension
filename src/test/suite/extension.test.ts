1dab8e462ae8211f1e8f7be395a826da7bced03c
jinghongluo <jil040@ucsd.edu>
2020-05-13 22:42:33 -0700

    revised extension test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,14 @@
 const vs = require('./vscode');
 var sinon = require('sinon');
+const act = require('./mockExtension');
 test("Same test", () => {
 	let x = 1 + 1;
 	expect(x).toBe(2);
 });
 
 test("Extension test", () =>{
-	const spy = sinon.spy(vs.window, "showInformationMessage");
-	console.log(spy.called); // false
-	vs.window.showInformationMessage("Test");
-	console.log(spy.called); // true
+	const spy = sinon.spy(vs.commands, "registerCommand");
+	act(vs);
 	expect(spy.called).toBe(true);
 	spy.restore();
 });
