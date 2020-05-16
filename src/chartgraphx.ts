a37c008f0101c1616c2c0d8ffecd85a904d1d172
oscarvik <oscarcv@stud.ntnu.no>
2020-05-15 19:30:37 -0700

    refactor: reset test framework
    change test framework from jest to mocha. enforce code style

--- src/chartgraphx.ts
+++ src/chartgraphx.ts
@@ -1,9 +1,9 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export const  displayCurrentWorkingFile = () => {
-    const rootPath = (vscode.workspace.rootPath !== undefined) ? vscode.workspace.rootPath: "";
-    
-    let openFileName = vscode.window.visibleTextEditors.map(e => e.document.fileName.replace(rootPath, ""));
-    let openFileLineCount = vscode.window.visibleTextEditors.map(e => e.document.lineCount);
-    vscode.window.showInformationMessage('Currently in file: ' + openFileName + "\n\nLines in file: "+openFileLineCount, { modal: true });
+    const rootPath = (vscode.workspace.rootPath !== undefined) ? vscode.workspace.rootPath : "";
+    const openFileName = vscode.window.visibleTextEditors.map((e) => e.document.fileName.replace(rootPath, ""));
+    const openFileLineCount = vscode.window.visibleTextEditors.map((e) => e.document.lineCount);
+    // tslint:disable-next-line: max-line-length
+    vscode.window.showInformationMessage("Currently in file: " + openFileName + "\n\nLines in file: " + openFileLineCount, { modal: true });
 };
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,20 @@
-import * as vscode from 'vscode';
-import {displayCurrentWorkingFile} from './chartgraphx';
+import * as vscode from "vscode";
+import {displayCurrentWorkingFile} from "./chartgraphx";
 
 export function activate(context: vscode.ExtensionContext) {
-	// This line of code will only be executed once when your extension is activated
-	console.log('ChartGraphX is now active!');
+    // This line of code will only be executed once when your extension is activated
 
-	const statusBarItem = vscode.window.createStatusBarItem(undefined, 1000);
-	statusBarItem.command = 'chartGraphX.displayState';
-	statusBarItem.tooltip = "run CGphX command";
-	statusBarItem.text = "CGphX$(rocket)"; // to rotate icon ~spin
-	statusBarItem.show();
-	let disposable = vscode.commands.registerCommand('chartGraphX.displayState', () => {
-		displayCurrentWorkingFile();
-	}
-	);
-	
-	context.subscriptions.push(disposable);
+    const statusBarItem = vscode.window.createStatusBarItem(undefined, 1000);
+    statusBarItem.command = "chartGraphX.displayState";
+    statusBarItem.tooltip = "run CGphX command";
+    statusBarItem.text = "CGphX$(rocket)"; // to rotate icon ~spin
+    statusBarItem.show();
+    const disposable = vscode.commands.registerCommand("chartGraphX.displayState", () => {
+        displayCurrentWorkingFile();
+    });
+    context.subscriptions.push(disposable);
 }
 
-
 // this method is called when your extension is deactivated
+// tslint:disable-next-line: no-empty
 export function deactivate() {}
