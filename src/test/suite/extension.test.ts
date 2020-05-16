ca1aabdf9e5c6157a9f4071a02b7fe912e321ac5
oscarvik <oscarcv@stud.ntnu.no>
2020-05-15 17:13:47 -0700

    added github action

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,12 +1,20 @@
-import * as assert from 'assert';
+const vs = require('./vscode');
+var sinon = require('sinon');
+test("Same test", () => {
+	let x = 1 + 1;
+	expect(x).toBe(2);
+});
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+test("Extension test", () =>{
+	const spy = sinon.spy(vs.window, "showInformationMessage");
+	console.log(spy.called); // false
+	vs.window.showInformationMessage("Test");
+	console.log(spy.called); // true
+	expect(spy.called).toBe(true);
+	spy.restore();
+});
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+
 
 	test('Sample test', () => {
 		assert.equal(-1, [1, 2, 3].indexOf(5));
