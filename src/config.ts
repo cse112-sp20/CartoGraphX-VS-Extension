efe3478065bc506bfdb581fb90fd91d5fc4c6b14
oscarvik <oscarcv@stud.ntnu.no>
2020-05-16 11:55:30 -0700

    fix config.ts: added firebase config for ChartGraphX

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,11 @@
 export const firebaseConfig = {
     // place firebase config here
+    apiKey: "AIzaSyA8jCEXgEsLljtkEUg-RCgHaF6i2cag_kY",
+    authDomain: "remote-13.firebaseapp.com",
+    databaseURL: "https://remote-13.firebaseio.com",
+    projectId: "remote-13",
+    storageBucket: "remote-13.appspot.com",
+    messagingSenderId: "113160307201",
+    appId: "1:113160307201:web:fc2dda4c33c5d2ce4ff600",
+    measurementId: "G-3F5MRDS2LK"
 };
