70f77af3c4f8e422030e8230563e87171e9747f6
pchang010 <phc010@ucsd.edu>
2020-05-18 18:01:23 -0700

    Added functionality to get list of files from remote master branch and repo name and url

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,13 @@ const auth = firebase.auth();
 
 // Set the global variable gitRoot to the root of the Git repository
 export let gitRoot : string = "";
+export let gitFilesArray : string[] = [];
+export let gitUrl : string = "";
+export let repoName : string = "";
 export let vscodeRoot :string | undefined = vscode.workspace.rootPath;
 export let git : any = simpleGit(vscodeRoot);
+
+
 /**
  * This function finds the Git root if the folder open in vscode is in a Git repository.
  */
@@ -29,8 +34,34 @@ async function findGitRoot() {
         vscode.window.showInformationMessage('The Git root directory is "' + gitRoot + '".');
     }
 }
-findGitRoot();
 
+
+/**
+ * This function gets an array of all the files in the remote master branch.
+ */
+async function findGitFiles() {
+    let gitFiles = await git.raw(['ls-tree', '-r', 'origin/master', '--full-tree', '--name-status']);
+    gitFilesArray = gitFiles.split('\n');
+    gitFilesArray.pop(); // Removes the last empty element from the array created from splitting on \n
+    console.log(gitFilesArray);
+}
+
+
+/**
+ * This function gets the url of the remote git repository.
+ */
+async function findGitUrl() {
+    gitUrl = await git.raw(['config', '--get', 'remote.origin.url']);
+    gitUrl = gitUrl.split('\n')[0];
+    repoName = gitUrl.split('.git')[0].split('/').slice(-1)[0];
+    console.log(gitUrl);
+    console.log(repoName);
+}
+
+
+findGitRoot();
+findGitFiles();
+findGitUrl();
 
 /**
  * This function is called when the extension is activated
