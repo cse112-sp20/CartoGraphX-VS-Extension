4944c915aeb10dd4f5ba785eb18bcbac0a9b081b
dharnant <35500434+dharnant@users.noreply.github.com>
2020-05-31 12:31:03 -0700

    docs(check_id_token): added comment describing checkIDToken function

--- cloud_functions/functions/src/check_id_token.js
+++ cloud_functions/functions/src/check_id_token.js
@@ -1,5 +1,13 @@
 const admin = require('firebase-admin');
 
+/**
+* Checks that the ID token in the header of the request 
+* that was sent is valid, gets the corresponding uid, and
+* sends that information to the desired cloud function.
+*
+* @param  {XMLHttpRequest} request The request that was passed in
+* @param  {function} next The cloud function that will be called after
+*/
 function checkIDToken(request, response, next){
   let idToken = request.header('idToken');
   admin.auth().verifyIdToken(idToken)
