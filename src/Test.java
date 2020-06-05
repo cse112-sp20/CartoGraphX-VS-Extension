1cb9426b2dbcd8ce728988309a5e2153d26acc1d
jinghongluo <jil040@ucsd.edu>
2020-06-05 11:11:10 -0700

    testing codacy

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+// This file is for testing purposes
+class Student{
+
+    // Declaring private fields
+    private String name;
+    private int id;
+    public Student(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+}
