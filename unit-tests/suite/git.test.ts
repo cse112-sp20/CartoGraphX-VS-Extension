d7a732c1c9c5ca905ed8083edba0f359a062cc43
pchang010 <phc010@ucsd.edu>
2020-06-10 05:10:07 -0700

    added unit tests for git functionality

--- unit-tests/suite/git.test.ts
+++ unit-tests/suite/git.test.ts
@@ -1,51 +1,81 @@
 import * as assert from "assert";
-import { findGitFileLines, findGitFiles, findGitRoot, findGitUrl, gitRoot, gitFilesArray, gitUrl, repoName, gitFileLines } from "../../src/git";
+import * as git from "../../src/git";
 
 /** This test suite tests the types of the exported variables from gitRoot */
 suite("testing git.ts", () => {
     
     /** testing if gitRoot is the correct type after running findGitRoot */
     test("assert that gitRoot is valid string", async () => {
-        findGitRoot();
-        assert.notEqual(undefined, gitRoot);
-        assert.equal(typeof "string", typeof gitRoot);
-        if (gitRoot.length === 0) {
-            assert.equal("", gitRoot);
+        let ret : string;
+        ret = await git.findGitRoot();
+        assert.notEqual(undefined, git.gitRoot);
+        assert.equal(typeof "string", typeof git.gitRoot);
+        assert.equal(git.gitRoot, ret);
+        if (git.gitRoot.length === 0) {
+            assert.equal("", git.gitRoot);
         }
+    });
+
+    /** testing that gitFetch returns correctly */
+    test("assert that gitFetch is valid string", async() => {
+        let ret : string;
+        assert.notEqual(undefined, git.gitFetch);
+        ret = await git.fetchRemoteGit();
+        assert.equal(git.gitFetch, ret);
     });
 
     /** testing if gitFilesArray is the correct type after running findGitFiles */
     test("assert that gitFilesArray is valid array", async () => {
-        findGitFiles();
-        assert.notEqual(undefined, gitFilesArray);
-        assert.equal(true, Array.isArray(gitFilesArray));
+        let ret : string [];
+        ret = await git.findGitFiles();
+        assert.notEqual(undefined, git.gitFilesArray);
+        assert.equal(git.gitFilesArray.length, ret.length);
     });
 
     /** testing if gitUrl is the correct type after running findGitUrl */
     test("assert that gitUrl is valid string", async () => {
-        findGitUrl();
-        assert.notEqual(undefined, gitUrl);
-        assert.equal(typeof "string", typeof gitUrl);
-        if (gitUrl.length === 0) {
-            assert.equal("", gitUrl);
+        let ret : string;
+        ret = await git.findGitUrl();
+        assert.notEqual(undefined, git.gitUrl);
+        assert.equal(typeof "string", typeof git.gitUrl);
+        if (git.gitUrl.length === 0) {
+            assert.equal("", git.gitUrl);
         }
+        assert.equal(git.gitUrl, ret);
     });
 
     /** testing if repoName is the correct type after running findGitUrl */
     test("assert that repoName is valid string", async () => {
-        findGitUrl();
-        assert.notEqual(undefined, repoName);
-        assert.equal(typeof "string", typeof repoName);
-        if (repoName.length === 0) {
-            assert.equal("", repoName);
+        await git.findGitUrl();
+        assert.notEqual(undefined, git.repoName);
+        assert.equal(typeof "string", typeof git.repoName);
+        if (git.repoName.length === 0) {
+            assert.equal("", git.repoName);
         }
     });
 
     /** testing if gitFileLines is the correct type after running findGitFileLines */
     test("testing findGitFileLines", async () => {
-        findGitFileLines();
-        assert.notEqual(undefined, gitFileLines);
-        assert.equal("object", typeof gitFileLines);
+        let ret : string;
+        ret = await git.findGitFileLines();
+        assert.notEqual(undefined, git.gitFileLines);
+        assert.equal("object", typeof git.gitFileLines);
+        assert.equal(git.gitFileLines, ret);
+    });
+
+    /** test sendGitData to the backend. This should fail because of an invalid token */
+    test("Testing sendGitData failure", async() => {
+        let ret : any = await git.sendGitData("badToken");
+        assert.equal(undefined, ret);
+    });
+
+    /** test if mapId setter works properly */
+    test("testing setMapID function", async() => {
+        assert.equal("", git.mapId);
+        git.setMapID("mapIdSample");
+        assert.equal("mapIdSample", git.mapId);
+        git.setMapID("m-132t91jhy2ihjuojdm][rt");
+        assert.equal("m-132t91jhy2ihjuojdm][rt", git.mapId);
     });
 
 });
